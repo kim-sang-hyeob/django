2aec966ca7cedcb10de004cf6601c98bd7696ad2
sanghyeob <fgd25@naver.com>
2021-10-25 21:46:11 +0900

    회사->물품리스트 출력 구현 완료

--- WebPage/mainapp/views.py
+++ WebPage/mainapp/views.py
@@ -13,20 +13,13 @@ def company_list(request):
     context={'company_list_all':company_list_all}
     return render(request ,'company_list.html' , context)
 
-
+# 여기서 막힘.
 def company_detail(request,id):
     company_id = Company_Data.objects.get(id=id)
-    context ={'company_id':company_id}
+    product = Product.objects.all()
+    context ={'company_id':company_id ,'product':product}
     return render(request, 'company_product.html', context) 
 
 
-'''def products(request,id):
-    product_id=get_object_or_404(Product,id=id)
-    product=Product.objects.all()
-    context = {'product':product, 'product_id':product_id}
-    return render(request , 'product.html' , context)
-
-    # 여기서 약간 헷갈림. detail처럼 구현하려고 했는데 어쨰서인지 구현이 잘 안됨.'''
-
-def product_list(request ):
+def product_list(request):
     pass 
